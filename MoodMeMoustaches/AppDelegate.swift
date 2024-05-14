871e21c70cf2ee76f97f6d08d2deeb1aa87bb493
Raul Pena <raulpen7@gmail.com>
2024-05-14 16:57:38 -0600

    Deleted whitespaces app delegate

--- MoodMeMoustaches/AppDelegate.swift
+++ MoodMeMoustaches/AppDelegate.swift
@@ -7,8 +7,6 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         SDImageCache.shared.config.maxDiskSize = 1000000 * 20
         FirebaseApp.configure()
@@ -28,7 +26,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
